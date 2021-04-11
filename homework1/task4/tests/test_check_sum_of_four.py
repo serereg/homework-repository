97bef3f2c2df1ca23d2eec8d39b4e7a7ffd714a7
serereg <serereg@mail.ru>
2021-04-12 00:39:47 +0300

    first test added

--- homework1/task4/tests/test_check_sum_of_four.py
+++ homework1/task4/tests/test_check_sum_of_four.py
@@ -1,2 +1,6 @@
 import pytest
+from check_sum_of_four.check_sum_of_four import check_sum_of_four
 
+def test_sum_of_two():
+    assert 3 == check_sum_of_four([-1, 0, 1], [-1, 0, 1], [0, 0, 0], [0, 0, 0])
+    

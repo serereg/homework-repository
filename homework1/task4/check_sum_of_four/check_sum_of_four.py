50296045776f13ac8214e37d9059556dd0ef2e74
serereg <serereg@mail.ru>
2021-04-13 00:44:55 +0300

    first version

--- homework1/task4/check_sum_of_four/check_sum_of_four.py
+++ homework1/task4/check_sum_of_four/check_sum_of_four.py
@@ -1,5 +1,15 @@
 from typing import List
+import itertools
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    return 3
+    result = 0
+
+    # to exclude equal numbers
+    short_a, short_b, short_c, short_d = set(a), set(b), set(c), set(d)
+    
+    for element in itertools.product(short_a, short_b, short_c, short_d):
+        if not sum(element):
+            result += 1
+
+    return result

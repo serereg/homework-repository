b4bfd7e2bda9b7d1e612dee5c8d2143d8392ac7e
serereg <serereg@mail.ru>
2021-04-14 19:52:30 +0300

    Task03 modified, and tests too
    Added comments in google style. Data files for tests copied to needed folder.

--- homework1/task03.py
+++ homework1/task03.py
@@ -25,7 +25,7 @@ def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
 
     Returns:
         Tuple[int, int]: [description]
-    """    
+    """
     min_value, max_value = None, None
     number_of_values = 0
     with open(file_name) as fi:
@@ -33,12 +33,10 @@ def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
             current_value = int(line)
             number_of_values += 1
             if number_of_values == 1:
+                max_value = min_value = current_value
+            elif current_value < min_value:
                 min_value = current_value
+            elif current_value > max_value:
                 max_value = current_value
-            else:
-                if current_value < min_value:
-                    min_value = current_value
-                if current_value > max_value:
-                    max_value = current_value
 
     return (min_value, max_value)

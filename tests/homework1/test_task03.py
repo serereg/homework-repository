e2ca9e2f1793b1889c4f00bd12609d1b7dae8afc
serereg <serereg@mail.ru>
2021-04-16 14:41:11 +0300

    Tests for task03 fixed

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -10,7 +10,7 @@ def _write_sequence_to_file(some_file: str, sequence: Sequence[int]):
         some_file (str): [description]
         sequence (Sequence[int]): [description]
     """
-    
+
     with open(some_file, "w") as fo:
         for s in sequence:
             fo.write(f"{s}\n")
@@ -35,24 +35,21 @@ def _read_sequence_from_file(some_file: str) -> Sequence[int]:
 
 
 def test_file_with_one_value():
-    """Testing file with one value
-    """
-    some_file = "test_data/task03_case1.txt"
+    """Testing file with one value"""
+    some_file = "tests/homework1/test_data/task03_case1.txt"
     sequence = _read_sequence_from_file(some_file)
     assert (min(sequence), max(sequence)) == find_maximum_and_minimum(some_file)
 
 
 def test_file_with_two_value():
-    """Testing file with two values
-    """
-    some_file = "test_data/task03_case2.txt"
+    """Testing file with two values"""
+    some_file = "tests/homework1/test_data/task03_case2.txt"
     sequence = _read_sequence_from_file(some_file)
     assert (min(sequence), max(sequence)) == find_maximum_and_minimum(some_file)
 
 
 def test_file_with_three_value():
-    """Testing file with three values
-    """
-    some_file = "test_data/task03_case3.txt"
+    """Testing file with three values"""
+    some_file = "tests/homework1/test_data/task03_case3.txt"
     sequence = _read_sequence_from_file(some_file)
     assert (min(sequence), max(sequence)) == find_maximum_and_minimum(some_file)

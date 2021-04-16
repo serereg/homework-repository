804768beb4f87f221ffd1b75bd2bff3cf1d6066b
serereg <serereg@mail.ru>
2021-04-16 15:34:08 +0300

    Error in task05 fixed, extratests added

--- homework1/task05.py
+++ homework1/task05.py
@@ -24,13 +24,23 @@ def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
     if k <= 0 or len(nums) == 0:
         raise ValueError("Not a list given to the function.")
 
-    result = 0
-    subarr = [0] * k
-    for num in nums:
-        subarr.pop(0)
-        subarr.append(num)
-        current_sum = sum(subarr)
+    result = current_sum = nums[0]
+    box_of_k_elements = 0
+    for ind in range(len(nums)):
+        if box_of_k_elements == 0:
+            result = current_sum = nums[0]
+        elif box_of_k_elements < k:
+            current_sum += nums[ind]
+        else:
+            current_sum += nums[ind]
+            current_sum -= nums[ind - k]
+        box_of_k_elements += 1
+        # print(f'{ind=}, {current_sum=}')
         if current_sum > result:
             result = current_sum
-
     return result
+
+
+if __name__ == "__main__":
+    find_maximal_subarray_sum([1, 3, -1, -3, 5, 3, 6, 7], 3)
+    find_maximal_subarray_sum([1, 2, 1, 6, 1, 1], 3)
--- tests/homework1/test_task05.py
+++ tests/homework1/test_task05.py
@@ -18,3 +18,13 @@ def test_empty_subarr():
     """Test of zero subarray"""
     with pytest.raises(ValueError):
         find_maximal_subarray_sum([1, 3, 1, 4], 0)
+
+
+def test_of_negative_array():
+    """Test of task sequense"""
+    assert -4 == find_maximal_subarray_sum([-20, -1, -2, -1, -30, -10, -10], 3)
+
+
+def test_of_positive_array():
+    """Test of task sequense"""
+    assert 9 == find_maximal_subarray_sum([1, 2, 1, 6, 1, 1], 3)

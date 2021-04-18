6be3d2e83e04f1a911648e49523e31ce9ebf09f3
serereg <serereg@mail.ru>
2021-04-18 22:05:56 +0300

    readlines() changed to readline()

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -4,32 +4,31 @@ from typing import Sequence
 from homework1.task03 import find_maximum_and_minimum
 
 
-def _read_sequence_from_file(some_file: str) -> Sequence[int]:
-    """Reading sequence from the file
-    """
+def test_file_with_one_value():
+    """Testing file with one value"""
+    some_file = os.path.dirname(__file__) + "/test_data/task03_case1.txt"
     sequence = []
     with open(some_file) as fi:
         for num in fi.readlines():
             sequence.append(int(num))
-    return sequence
-
-
-def test_file_with_one_value():
-    """Testing file with one value"""
-    some_file = os.path.dirname(__file__) + "/test_data/task03_case1.txt"
-    sequence = _read_sequence_from_file(some_file)
     assert (min(sequence), max(sequence)) == find_maximum_and_minimum(some_file)
 
 
 def test_file_with_two_value():
     """Testing file with two values"""
     some_file = os.path.dirname(__file__) + "/test_data/task03_case2.txt"
-    sequence = _read_sequence_from_file(some_file)
+    sequence = []
+    with open(some_file) as fi:
+        for num in fi.readlines():
+            sequence.append(int(num))
     assert (min(sequence), max(sequence)) == find_maximum_and_minimum(some_file)
 
 
 def test_file_with_three_value():
     """Testing file with three values"""
     some_file = os.path.dirname(__file__) + "/test_data/task03_case3.txt"
-    sequence = _read_sequence_from_file(some_file)
+    sequence = []
+    with open(some_file) as fi:
+        for num in fi.readlines():
+            sequence.append(int(num))
     assert (min(sequence), max(sequence)) == find_maximum_and_minimum(some_file)

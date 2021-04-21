238f944cb5d49131c8e975ff865fba98d965980e
serereg <serereg@mail.ru>
2021-04-22 01:55:56 +0300

    hw5 beginning

--- homework2/hw5.py
+++ homework2/hw5.py
@@ -5,7 +5,6 @@ positional and keyword arguments.
 Write a function that accept any iterable of unique values and then
 it behaves as range function:
 
-
 import string
 
 
@@ -14,3 +13,25 @@ assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 
 assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
 
 """
+def custom_range(iter, *args):
+    """function that accept any iterable of unique values and then
+    it behaves as range function
+    Args:
+        iter ([type]): [description]
+    """
+    if len(args) == 1:
+        start, stop, step = args[0], args[0], 1
+    if len(args) == 2:
+        start, stop, step = args[0], args[1], 1
+    if len(args) == 3:
+        start, stop, step = args
+
+    print(start, stop, step)
+    # iter.reverse()
+    # for i, k in enumerate(iter):
+        # print(i, k)
+    # a = list(1, 2, 3, 4)
+    a = iter.index(start)
+    b = iter.index(stop)
+    return iter[a:b:step]
+    

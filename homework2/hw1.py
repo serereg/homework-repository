3872fa8f12f03c609a315ec59420f0e437ac85ad
serereg <serereg@mail.ru>
2021-04-26 13:49:35 +0300

    trash comments are removed

--- homework2/hw1.py
+++ homework2/hw1.py
@@ -63,24 +63,6 @@ def get_rarest_char(file_path: str) -> str:
             for char in line:
                 counts_of_chars[char] = counts_of_chars.get(char, 0) + 1
 
-    # 1 way
-    # li = [(item, counts_of_chars[item]) for item
-    # in counts_of_chars.keys()]
-    # li.sort(key=lambda el: el[1])
-    # rarest_char = li[0][0]
-
-    # 2 way
-    # sorted_counts_of_chars = sorted(counts_of_chars,
-    # key=counts_of_chars.get)  # type: ignore
-    # error: Argument "key" to "sorted" has incompatible type
-    # overloaded function; expected "Callable[[str], SupportsLessThan]"
-    # Found 1 error in 1 file (checked 3 source files)
-
-    # 3 way
-    # sorted_counts_of_chars = sorted(counts_of_chars.items(),
-    # key=lambda kv: kv[1])
-
-    # 4 way
     sorted_counts_of_chars = [
         k for k, v in sorted(counts_of_chars.items(), key=operator.itemgetter(1))
     ]
@@ -90,15 +72,7 @@ def get_rarest_char(file_path: str) -> str:
 def count_punctuation_chars(file_path: str) -> int:
     """Count every punctuation char"""
     counter_of_punctuation = 0
-    # punctuations = set(string.punctuation)
     with open(file_path, encoding="unicode_escape") as fi:
-        # 1 way
-        # for line in fi:
-        #     for char in line:
-        #         if char in punctuations:
-        #             counter_of_punctuation += 1
-
-        # 2 way
         counters_of_symbols: Dict[str, int] = {}
         for line in fi:
             for symbol in line:
@@ -124,9 +98,6 @@ def count_non_ascii_chars(file_path: str) -> int:
     counter_of_non_ascii = 0
     with open(file_path, encoding="unicode_escape") as fi:
         for line in fi:
-            # a = len(line)
-            # b = len(line.encode("ascii", "ignore"))
-            # counter_of_non_ascii += a - b
             for char in line:
                 if ord(char) > 127:
                     counter_of_non_ascii += 1
@@ -141,20 +112,10 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
             for symbol in line:
                 counters_of_symbols[symbol] = counters_of_symbols.get(symbol, 1) + 1
         non_ascii_chars: Dict[str, int] = {}
-        # non_ascii_chars = {key: value for key, value in
-        # counters_of_symbols if key
-        # not in string.ascii_letters}
         for key in counters_of_symbols.keys():
             if key not in string.printable:
                 non_ascii_chars[key] = counters_of_symbols[key]
         print(non_ascii_chars)
-        # most_common = max(non_ascii_chars, key=non_ascii_chars.get)
-        # mypy generates error if use max...
-        # sorted_non_ascii_chars = sorted(
-        #     non_ascii_chars.items(), key=lambda kv: kv[1],
-        # reverse=True
-        # )
-        # most_common = sorted_non_ascii_chars[0][0]
         sorted_non_ascii_chars = [
             k for k, v in sorted(non_ascii_chars.items(), key=operator.itemgetter(1))
         ]

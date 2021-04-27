9c1f72580491488ff9a27e9c2296d34d11cf2bf1
serereg <serereg@mail.ru>
2021-04-27 23:56:36 +0300

    task02 first version

---
+++ homework3/task02/task02.py
@@ -0,0 +1,27 @@
+"""Here's a not very efficient calculation function
+that calculates something important::
+
+Calculate total sum of slow_calculate() of all numbers
+starting from 0 to 500.
+Calculation time should not take more than a minute.
+Use functional capabilities of multiprocessing module.
+You are not allowed to modify slow_calculate function.
+"""
+
+import hashlib
+import multiprocessing
+import random
+import struct
+import time
+
+
+def slow_calculate(value):
+    """Some weird voodoo magic calculations"""
+    time.sleep(random.randint(1, 3))
+    data = hashlib.md5(str(value).encode()).digest()
+    return sum(struct.unpack("<" + "B" * len(data), data))
+
+
+if __name__ == "__main__":
+    with multiprocessing.Pool(500) as p:
+        print(sum(p.map(slow_calculate, [i for i in range(500)])))

180d87dd9c5c52fe442be724256c725f5b99487a
serereg <serereg@mail.ru>
2021-05-01 23:54:28 +0300

    first version homework4

--- homework4/task_1_read_file.py
+++ homework4/task_1_read_file.py
@@ -43,5 +43,5 @@ def read_magic_number(path: str) -> bool:
         with open(path) as fi:
             return 1.0 <= float(fi.readline()) < 3.0
             # TODO: use rounding
-    except (FileNotFoundError, IOError):
+    except (FileNotFoundError, IOError, ValueError):
         raise ValueError("Error while reading a number")

4f2b7860f0c14f0cf600ba2414612d93c0fea76a
serereg <serereg@mail.ru>
2021-05-03 21:51:03 +0300

    mock is used for testing cached function

--- tests/homework3/test_task01.py
+++ tests/homework3/test_task01.py
@@ -1,7 +1,9 @@
+from unittest.mock import Mock, call
+
 from homework3.task01 import cache_queue
 
 
-def testing_smoke():
+def test_smoke():
     """Testing usual behaviour"""
 
     @cache_queue(times=2)
@@ -15,3 +17,13 @@ def testing_smoke():
     assert 10009 == func(*some, c=4, d=5)
     assert 10009 == func(*some, c=4, d=5)
     # TODO: some enhancements should be done according to lecture04
+
+
+def test_cached():
+    mock = Mock()
+    cached_function = cache_queue(times=2)(mock)
+    cached_function(1, 3)
+    cached_function(1, 3)
+    cached_function(1, 3)
+    cached_function(1, 3)
+    assert mock.mock_calls == [call(1, 3), call(1, 3)]

3508c5c2995b63b03d4833c2d093ed9345642f03
serereg <serereg@mail.ru>
2021-05-14 01:20:12 +0300

    reformatting files and test added

--- tests/homework5/test_task01.py
+++ tests/homework5/test_task01.py
@@ -3,7 +3,6 @@ import pytest
 
 from homework5.oop_1 import Homework, Student, Teacher
 
-
 FAKE_TIME = datetime.datetime(2020, 12, 25, 17, 5, 55)
 
 
@@ -21,9 +20,6 @@ def patch_datetime_now(monkeypatch):
     monkeypatch.setattr(datetime, "datetime", MyDatetime)
 
 
-# TODO: to write different tests for different classes
-
-
 def test_creating_objects():
     teacher = Teacher("Daniil", "Shadrin")
     student = Student("Roman", "Petrov")
@@ -34,9 +30,9 @@ def test_creating_objects():
 def test_creating_homework_by_teacher():
     teacher = Teacher("Daniil", "Shadrin")
     expired_homework = teacher.create_homework("Learn functions", 0)
+    assert isinstance(expired_homework, Homework)
     assert expired_homework.deadline == datetime.timedelta(0)
     assert expired_homework.text == "Learn functions"
-    assert isinstance(expired_homework, Homework)
 
 
 def test_working_with_time_in_homework(patch_datetime_now):
@@ -50,5 +46,14 @@ def test_creating_function_from_method_and_using_it():
     create_homework_too = Teacher.create_homework
     oop_homework = create_homework_too("create 2 simple classes", 5)
     assert oop_homework.deadline == datetime.timedelta(days=5)
-    # student.do_homework(oop_homework)
-    # student.do_homework(expired_homework)  # You are late
+
+
+def test_do_homework_with_expired_homework_and_not_expired(capsys):
+    teacher = Teacher("Daniil", "Shadrin")
+    expired_homework = teacher.create_homework("Learn functions", 0)
+    oop_homework = teacher.create_homework("create 2 simple classes", 5)
+    student = Student("Roman", "Petrov")
+    student.do_homework(expired_homework)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "You are late"
+    assert oop_homework == student.do_homework(oop_homework)

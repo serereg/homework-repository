b575163e1a6577cadf03754486fbfba65ed36961
serereg <serereg@mail.ru>
2021-05-25 12:06:45 +0300

    tests for hw1 are expanded

--- tests/homework7/test_hw1.py
+++ tests/homework7/test_hw1.py
@@ -2,7 +2,6 @@ from homework7.hw1 import find_occurrences
 
 
 def test_usual_tree():
-    # Example tree:
     example_tree = {
         "first": ["RED", "BLUE"],
         "second": {
@@ -21,3 +20,51 @@ def test_usual_tree():
     }
     cnt = find_occurrences(example_tree, "RED")
     assert cnt == 6
+
+
+def test_usual_tree2():
+    example_tree = {
+        "first": ["RED", "BLUE"],
+        "second": {
+            "simple_key": ["RED", "list", "of", "RED", "valued"],
+        },
+        "third": {
+            "abc": "RED",
+            "jhl": "RED",
+            "complex_key": {
+                "key1": "value1",
+                "key2": "RED",
+                "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
+            },
+        },
+        "fourth": "RED",
+    }
+    cnt = find_occurrences(example_tree, "RED")
+    assert cnt == 8
+
+
+def test_usual_tree3():
+    example_tree = {
+        "first": ["RED", "BLUE"],
+        "second": {
+            "simple_key": ["RED", "list", "of", "RED", "valued"],
+        },
+        "third": {
+            "abc": "RED",
+            "jhl": "RED",
+            "complex_key": {
+                "key1": "value1",
+                "key2": "RED",
+                "key3": [
+                    "a",
+                    "RED",
+                    "of",
+                    "values",
+                    {"nested_key": "RED", "abd": "xyz", "qwe": ["a", "RED"]},
+                ],
+            },
+        },
+        "fourth": "RED",
+    }
+    cnt = find_occurrences(example_tree, "RED")
+    assert cnt == 10

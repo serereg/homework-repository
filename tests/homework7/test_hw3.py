97a5e31b0a456ec001161dbe230ed5dadf4ebfd7
serereg <serereg@mail.ru>
2021-05-27 11:49:12 +0300

    tests added

--- tests/homework7/test_hw3.py
+++ tests/homework7/test_hw3.py
@@ -1,38 +1,75 @@
 from homework7.hw3 import tic_tac_toe_checker
 
 
-def test_x_wins():
-    board1 = [["-", "-", "o"], ["-", "o", "o"], ["x", "x", "x"]]
-    assert "x wins!" == tic_tac_toe_checker(board1)
+def test_x_or_o_wins():
+    # TODO: is it right way to generate combinations automatically?
+    #  also use mark.parametrize
 
-    board2 = [["x", "-", "o"], ["x", "o", "o"], ["x", "o", "x"]]
+    w, l, u = "x", "o", "-"
+    # fmt: off
+    board_w_wins_horizontal = [[[u, u, l],
+                                [u, l, l],
+                                [w, w, w]],
 
-    assert "x wins!" == tic_tac_toe_checker(board2)
+                               [[u, u, l],
+                                [w, w, w],
+                                [w, w, l]],
 
+                               [[w, w, w],
+                                [w, u, l],
+                                [w, w, l]]]
 
-def test_o_wins():
-    board1 = [["-", "-", "o"], ["-", "o", "x"], ["o", "o", "o"]]
-    assert "o wins!" == tic_tac_toe_checker(board1)
+    board_w_wins_vertical = [[[w, l, l],
+                              [w, u, l],
+                              [w, l, w]],
 
-    board2 = [["-", "-", "o"], ["-", "o", "o"], ["x", "x", "o"]]
-    assert "o wins!" == tic_tac_toe_checker(board2)
+                             [[u, w, l],
+                              [w, w, l],
+                              [l, w, w]],
 
-    assert "o wins!" == tic_tac_toe_checker(board2)
+                             [[u, u, w],
+                              [w, l, w],
+                              [l, w, w]]]
+
+    board_w_wins_diagonal = [[[w, u, l],
+                              [u, w, l],
+                              [u, l, w]],
+
+                             [[l, u, w],
+                              [u, w, l],
+                              [w, u, u]]]
+    # fmt: on
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_horizontal[0])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_horizontal[1])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_horizontal[2])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_vertical[0])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_vertical[1])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_vertical[2])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_diagonal[0])
+    assert "x wins!" == tic_tac_toe_checker(board_w_wins_diagonal[1])
 
 
 def test_draw():
-    board1 = [["o", "x", "o"], ["x", "o", "o"], ["x", "o", "x"]]
-    assert "draw!" == tic_tac_toe_checker(board1)
+    # fmt: off
+    board1 = [["o", "x", "o"],
+              ["x", "o", "o"],
+              ["x", "o", "x"]]
 
+    # fmt: on
     board2 = [["x", "o", "o"], ["o", "x", "x"], ["x", "o", "o"]]
-
+    assert "draw!" == tic_tac_toe_checker(board1)
     assert "draw!" == tic_tac_toe_checker(board2)
 
 
 def test_unfinished():
-    board1 = [["-", "x", "o"], ["-", "o", "o"], ["-", "o", "x"]]
+    # fmt: off
+    board1 = [["-", "x", "o"],
+              ["-", "o", "o"],
+              ["-", "o", "x"]]
+
+    board2 = [["-", "-", "o"],
+              ["o", "-", "o"],
+              ["x", "-", "x"]]
+    # fmt: on
     assert "unfinished" == tic_tac_toe_checker(board1)
-
-    board2 = [["-", "-", "o"], ["o", "-", "o"], ["x", "-", "x"]]
-
     assert "unfinished" == tic_tac_toe_checker(board2)

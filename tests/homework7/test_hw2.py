07f2e3b18bbad7715516fa17e623467164512673
serereg <serereg@mail.ru>
2021-05-29 15:55:23 +0300

    additional variable added for getting more information while testing

--- tests/homework7/test_hw2.py
+++ tests/homework7/test_hw2.py
@@ -2,18 +2,29 @@ from homework7.hw2 import backspace_compare
 
 
 def test_equal_strings():
-    assert backspace_compare("asfd sfd fd 123", "asfd sfd fd 123") is True
-    assert backspace_compare("12344#56", "1222##3456") is True
+    result = backspace_compare("asfd sfd fd 123", "asfd sfd fd 123")
+    assert result is True
+    result = backspace_compare("12344#56", "1222##3456")
+    assert result is True
+    result = backspace_compare("#12344#56", "1222##3456")
+    assert result is True
 
 
 def test_empty_strings():
-    assert backspace_compare("####", "##") is True
-    assert backspace_compare("", "") is True
-    assert backspace_compare("12##", "1234####") is True
+    result = backspace_compare("####", "##")
+    assert result is True
+    result = backspace_compare("", "")
+    assert result is True
+    result = backspace_compare("12##", "1234####")
+    assert result is True
 
 
 def test_non_equal_strings():
-    assert backspace_compare("1234", "12") is False
-    assert backspace_compare("12", "1234") is False
-    assert backspace_compare("a", "ba") is False
-    assert backspace_compare("ba", "a") is False
+    result = backspace_compare("1234", "12")
+    assert result is False
+    result = backspace_compare("12", "1234")
+    assert result is False
+    result = backspace_compare("a", "ba")
+    assert result is False
+    result = backspace_compare("ba", "a")
+    assert result is False

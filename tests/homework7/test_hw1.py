e283d051972a5ec356ff3a57245a5b126eafe713
serereg <serereg@mail.ru>
2021-05-29 16:50:08 +0300

    tests added

--- tests/homework7/test_hw1.py
+++ tests/homework7/test_hw1.py
@@ -64,3 +64,55 @@ def test_usual_tree():
     }
     cnt = find_occurrences(example_tree, "RED")
     assert cnt == 10
+
+
+def test_empty_tree():
+    tree = {}
+    cnt = find_occurrences(tree, "RED")
+    assert cnt == 0
+    cnt = find_occurrences(tree, "")
+    assert cnt == 0
+
+
+def test_with_one_node():
+    tree = {"first": "RED"}
+    cnt = find_occurrences(tree, "RED")
+    assert cnt == 1
+    tree = {"first": ""}
+    cnt = find_occurrences(tree, "")
+    assert cnt == 1
+    tree = {False: False}
+    cnt = find_occurrences(tree, False)
+    assert cnt == 1
+
+
+def test_tree_with_different_types_and_nesting():
+    tree = {
+        False: False,
+        True: 0,
+        1: False,
+        0: 0.0,
+        0.0: 0,
+        frozenset({1, 2, 3}): {1, 2, 3},
+        frozenset({4, 5, 6}): {frozenset({1, 2, 3}): {1, 2, 3}, False: {7, 8, 9}},
+        (0, 0): [(0, 0), (0, 1), (0, 1)],
+    }
+    cnt = find_occurrences(tree, False)
+    assert cnt == 2
+    cnt = find_occurrences(tree, {1, 2, 3})
+    assert cnt == 2
+    cnt = find_occurrences(tree, (7, 8, 9))
+    assert cnt == 0
+    cnt = find_occurrences(tree, {7, 8, 9})
+    assert cnt == 1
+    cnt = find_occurrences(tree, 0)
+    assert cnt == 2  # could be 5
+    cnt = find_occurrences(tree, (0, 0))
+    assert cnt == 1
+    cnt = find_occurrences(tree, (0, 1))
+    assert cnt == 2
+
+
+# 3. Дерево где есть невалидные данные
+# 5. Разной вложенности
+# и так далее

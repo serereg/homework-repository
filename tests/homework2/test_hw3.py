b2e7c4222e3aa6e28451bd6a92772b7c7b1c1124
serereg <serereg@mail.ru>
2021-06-01 10:08:57 +0300

    version for the mentor

--- tests/homework2/test_hw3.py
+++
@@ -1,8 +0,0 @@
-from typing import Sequence
-
-from homework2.hw3 import combinations
-
-
-def test_smoke():
-    all_combinations = combinations([1, 2], [4, 3])
-    assert all_combinations == [[1, 4], [1, 3], [2, 4], [2, 3]]

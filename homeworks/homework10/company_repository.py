75689aae83fbd6bd51b0750aa5a6bfd94a88de6c
serereg <serereg@mail.ru>
2021-06-16 21:24:18 +0300

    monkeypatch added

--- homeworks/homework10/company_repository.py
+++ homeworks/homework10/company_repository.py
@@ -1,6 +1,6 @@
 import aiohttp
-
 import asyncio
+import math
 import string
 
 from bs4 import BeautifulSoup
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 class CompanyRepository:
     def __init__(self, url="https://markets.businessinsider.com"):
-        self._common_lists_of_companies_pages = []
+        self._common_lists_of_companies_pages = set()
         self._cache = []
         self._url = url
 
@@ -16,8 +16,17 @@ class CompanyRepository:
             f"""{self._url}/index/components/s&p_500?p={p}""" for p in range(1, 11)
         ]
 
-    @staticmethod
-    async def _parse_company_list(page_with_table):
+    async def _parse_company_list(self, page_with_table: str):
+        """Parse one of several pages with list of companies
+
+        Args:
+            page_with_table: page source
+
+        Returns:
+            Blob with main parameters of company, i.e.
+                Name, URL, Growth, Price, Code "P/E Ratio",
+                "52 Week Low", "52 Week High".
+        """
         soup = BeautifulSoup(page_with_table, "lxml")
 
         # TODO: check columns names
@@ -30,14 +39,14 @@ class CompanyRepository:
 
         for row in table_rows:
             name = row.find("td", {"class": "table__td table__td--big"}).find("a").text
-            link = "https://markets.businessinsider.com" + row.find("a").get("href")
+            link = self._url + row.find("a").get("href")
             growth = row.find_all("td")[7].text.split()[1]
 
-            company_page = await CompanyRepository._fetch(link)
+            company_page = await CompanyRepository._fetch_company(link)
 
             yield {
                 "Name": name,
-                "url": link,
+                "URL": link,
                 "Growth": growth,
                 **CompanyRepository._parse_company_page(company_page),
             }
@@ -57,15 +66,16 @@ class CompanyRepository:
         right_side_of_page = soup.find(
             "div", {"class": "site-content__col site-content__col--right"}
         ).find_all("div", {"class": "snapshot__data-item"})
-        table = {
-            item.find("div").text: float(item.text.strip().split()[0])
-            for item in right_side_of_page
-        }
-
-        pne = table["P/E Ratio"]
-        week52low = table["52 Week Low"]
-        week52high = table["52 Week High"]
-
+        try:
+            table = {
+                item.find("div").text.strip(): float(item.text.strip().split()[0])
+                for item in right_side_of_page
+            }
+        except ValueError:
+            table = {}
+        pne = table.get("P/E Ratio", math.nan)
+        week52low = table.get("52 Week Low", math.nan)
+        week52high = table.get("52 Week High", math.nan)
         return {
             "Price": price,
             "Code": code,
@@ -82,6 +92,16 @@ class CompanyRepository:
                     raise ValueError(f"Can't open url: {url}")
                 return await resp.text()
 
+    @staticmethod
+    async def _fetch_company(url: str):
+        """Mock in easy way of _fetch method"""
+        return await CompanyRepository._fetch(url)
+
+    @staticmethod
+    async def _fetch_company_list(url: str):
+        """Mock in easy way of _fetch method"""
+        return await CompanyRepository._fetch(url)
+
     async def get_all_companies(self):
         if self._cache:
             for item in self._cache:
@@ -90,7 +110,9 @@ class CompanyRepository:
             # create a list of tasks to execute
             # get_data_page(page_number) for page_number in range(10)
             for url in self.urls:
-                self._common_lists_of_companies_pages.append(await self._fetch(url))
+                self._common_lists_of_companies_pages.add(
+                    await CompanyRepository._fetch_company_list(url)
+                )
 
         for list_page in self._common_lists_of_companies_pages:
             # TODO: use yield from
@@ -108,5 +130,6 @@ async def foo():
     await asyncio.sleep(10)
 
 
-asyncio.run(foo())
-print("finish")
+if __name__ == "__main__":
+    asyncio.run(foo())
+    print("finish")
--- tests/homework10/test_stonks.py
+++ tests/homework10/test_stonks.py
@@ -1,25 +1,58 @@
+import asyncio
+
+# import pytest
+
+# from unittest.mock import Mock
 from pathlib import Path
 
-from homeworks.homework10.train1 import Company, parse_list_of_companies
+from homeworks.homework10.company_repository import CompanyRepository
 
 
-def test_parse_list_of_companies():
-    path = (
-        Path(__file__).parent / "data/S&P 500 Stock _ S&P 500 Comp"
-        "anies _ S&P 500 Value _ Markets Insider.html"
-    )
-    pages_src = [path.read_text()]
-    companies = parse_list_of_companies(pages_src)
-    print(companies)
-    assert False
+async def smoke_company_repository():
+    repo = CompanyRepository()
+    async for company in repo.get_all_companies():
+        print(company)
 
 
-def test_parse_company():
-    path = (
-        Path(__file__).parent
-        / "data/MMM Stock _ 3M Stock Price Today _ Markets Insider.html"
-    )
-    blob = path.read_text()
-    company = Company.from_blob(blob)
-    print(company)
-    assert False
+async def fetch_company(url: str) -> str:
+    path = Path(__file__).parent / "data/MMM Stock _ 3M.html"
+    return path.read_text()
+
+
+async def fetch_company_list(url: str) -> str:
+    path = Path(__file__).parent / "data/S&P 500 Stock.html"
+    return path.read_text()
+
+
+def test_company_repository(monkeypatch):
+    monkeypatch.setattr(CompanyRepository, "_fetch_company", fetch_company)
+    monkeypatch.setattr(CompanyRepository, "_fetch_company_list", fetch_company_list)
+    asyncio.run(smoke_company_repository())
+    print("finish")
+    assert True
+
+
+# def test_parse_list_of_companies():
+#     path = (
+#             Path(__file__).parent /
+#             "data/S&P 500 Stock _ S&P 500 Comp"
+#                                     "anies _ S&
+#             P 500 Value _ Markets Insider.html"
+#     )
+#     pages_src = [path.read_text()]
+#     companies = parse_list_of_companies(pages_src)
+#     print(companies)
+#     assert False
+
+
+# def test_parse_company():
+#     path = (
+#             Path(__file__).parent
+#             / "data/MMM Stock _ 3M Stock
+#             Price Today _ Markets Insider.html"
+#     )
+#     blob = path.read_text()
+#     company = Company.from_blob(blob)
+#     print(company)
+#     assert False
+#

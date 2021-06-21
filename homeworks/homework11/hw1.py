79a95542f3f5dc30fdeeebaa32cde9d32589892e
serereg <serereg@mail.ru>
2021-06-21 15:17:53 +0300

    hw1 first version

--- homeworks/homework11/hw1.py
+++ homeworks/homework11/hw1.py
@@ -33,3 +33,30 @@ class SizesEnum(metaclass=SimplifiedEnum):
 assert ColorsEnum.RED == "RED"
 assert SizesEnum.XL == "XL"
 """
+
+
+class SimplifiedEnum(type):  # is a metaclass
+    """Return a class with attributes from a __keys attribute.
+
+    Example:
+        class ColorsEnum(metaclass=SimplifiedEnum):
+            __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+        class SizesEnum(metaclass=SimplifiedEnum):
+            __keys = ("XL", "L", "M", "S", "XS")
+
+
+        assert ColorsEnum.RED == "RED"
+        assert SizesEnum.XL == "XL"
+    """
+
+    def __new__(cls, name, bases, dct):
+        cls_instance = super().__new__(cls, name, bases, dct)
+        try:
+            attrs = [*dct[f"_{cls_instance.__name__}__keys"]]
+            for attr in attrs:
+                setattr(cls_instance, attr, attr)
+            return cls_instance
+        except KeyError as k:
+            raise KeyError("Class must have __key attribute") from k

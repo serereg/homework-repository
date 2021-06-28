fb79d54a4299446555804682d452f8492cd42e1d
serereg <serereg@mail.ru>
2021-06-28 23:13:47 +0300

    first version hw2

---
+++ homeworks/homework11/hw2.py
@@ -0,0 +1,53 @@
+"""
+You are given the following code:
+
+ class Order:
+     morning_discount = 0.25
+
+     def __init__(self, price):
+         self.price = price
+
+     def final_price(self):
+         return self.price - self.price * self.morning_discount
+
+Make it possible to use different discount programs.
+Hint: use strategy behavioural OOP pattern.
+https://refactoring.guru/design-patterns/strategy
+
+Example of the result call:
+
+def morning_discount(order):
+     ...
+
+def elder_discount(order):
+     ...
+
+>>> order_1 = Order(100, morning_discount)
+>>> assert order_1.final_price() == 50
+
+>>> order_2 = Order(100, elder_discount)
+>>> assert order_2.final_price() == 10
+"""
+
+
+class Order:
+    def __init__(self, price, strategy):
+        self.strategy = strategy
+        self.price = price
+
+    def final_price(self):
+        return self.price - self.strategy(self)
+
+
+def morning_discount(order):
+    return 0.5 * order.price
+
+
+def elder_discount(order):
+    return 0.9 * order.price
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
